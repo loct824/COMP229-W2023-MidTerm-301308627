6debd0e1dc697ad064631cad545dbd32dd643136
loct <loct824@gmail.com>
2023-03-01 12:29:24 -0500

    azure db

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/midterm"
+//   "URI": "mongodb://localhost/midterm"
+"URI": 'mongodb://loct824:Adg5mqKgW0cKNYOOd8RuI1i3PO7ftntW7uHfH6skFYQoCwbNaYEabANqgjdTB98iNOl3drnUDWS2ACDb63k3hw==@loct824.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@loct824@/midterm'
 };
